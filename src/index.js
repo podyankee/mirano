daeadee1c61fc34375b511d2d3cf4260201549ed
Kostyantin <podyankee@gmail.com>
2024-05-12 08:58:49 +0300

    day 6 preloader added

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import { initHeaderFixer } from '@/scripts/headerFixer';
 import { initChoices } from '@/scripts/choices';
 import { initCart } from '@/scripts/cart';
 import { renderProducts } from '@/scripts/renderProducts';
-import { store } from './scripts/Store';
-import { initChoicesType } from './scripts/choicesType';
-import { filterProducts } from './scripts/filterProducts';
+import { store } from '@/scripts/Store';
+import { initChoicesType } from '@/scripts/choicesType';
+import { filterProducts } from '@/scripts/filterProducts';
+import { initSearchProducts } from '@/scripts/searchProducts';
 
 const init = () => {
 	initHeaderFixer();
 	initChoices();
 	initChoicesType();
 	initCart();
+	initSearchProducts();
 	renderProducts();
 	filterProducts();
 };
